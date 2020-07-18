6e0a6f447309bbc01ce1632122d2a50203578379
Highlander Dantas <haylander60@gmail.com>
2020-07-17 22:13:35 -0300

    update

--- dist/src/repository/newsRepository.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require("mongoose");
-const news_1 = require("../model/news");
-exports.default = mongoose.model("news", news_1.default);
